95d2eae57b5e26122145aebd8d545d3c651dc25c
TBgames1 <tgbledsoe07@gmail.com>
2020-09-02 16:27:54 -0500

    Really cool physics based movement. Plus sprinting. Very configurable.

---
+++ Assets/Scripts/PhysicsManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsManager : MonoBehaviour
+{
+    [Tooltip("9.81 is the value of Earth's Gravity and is the default value for this game.")] [SerializeField] float m_gravity = 9.81f;
+
+    private void FixedUpdate()
+    {
+        Physics.gravity = new Vector3(0, -m_gravity, 0);
+
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,26 +4,90 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] float m_Speed = 7f;
+    [SerializeField] float m_rotationSpeed = 5f;
+    [SerializeField] float m_jumpForce = 10f;
+    [SerializeField] float m_sprintBoost = .2f;
+
     private bool m_isIdle = true;
     private bool m_isRunning = false;
     private Animator m_animator;
-    [SerializeField] float m_Speed = 7f;
-    // Start is called before the first frame update
+    private Rigidbody m_rigidBody;
+    private bool m_isTouchingGround = true;
+    private bool m_isSprinting = false;
+    private bool m_isJumping = false;
     void Start()
     {
         m_animator = GetComponent<Animator>();
+        m_rigidBody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        float speed = m_Speed;
+
+        //Sprint
+        if (Input.GetButton("Sprint"))
+        {
+            print("Sprinting...");
+            speed = m_Speed + (m_Speed * m_sprintBoost);
+        }
+
+        //Do movement
+        Vector3 movemment = new Vector3(Input.GetAxis("Horizontal") * speed, m_rigidBody.velocity.y, Input.GetAxis("Vertical") * speed);
+
+        
+        //Rotate to direction
+        if (!(movemment.x == 0f && movemment.z == 0f))
+        {
+            m_isIdle = true;
+            transform.rotation = Quaternion.Slerp(transform.rotation,Quaternion.LookRotation(movemment), Time.deltaTime * m_rotationSpeed);
+        }
+        else
+        {
+            m_isIdle = false;
+        }
+
+        //Actually move character
+        m_rigidBody.velocity = movemment;
+
+        //Jump
+        if (Input.GetButtonDown("Jump") && !m_isJumping)
+        {
+
+            m_rigidBody.AddForce(new Vector3(0, m_jumpForce, 0), ForceMode.Impulse);
+            m_isTouchingGround = false;
+            m_isJumping = true;
+        }
+  
+        //Handle Jumping Animation:
+        if (!m_isTouchingGround || transform.position.y > 1.5f)
+        {
+            m_animator.SetBool("isJumping", true);
+        }
+        else
+        {
+            m_animator.SetBool("isJumping", false);
+            m_isJumping = false;
+        }
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //8 is the ground
+        if(collision.gameObject.layer == 8)
+        {
+            //Turn off jumping animation
+            m_isTouchingGround = true;
+        }
+    }
+
     void Update()
     {
-        if (m_isIdle)
-        {
-            m_animator.SetBool("isRunning", !m_isIdle);
-        }
-        if(Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
-        {
-         //   gameObject.transform.position = new Vector3();
-        }
+        //Update Animation depending on if the player is idle.
+        m_animator.SetBool("isRunning", m_isIdle);
+       
+
     }
 }
