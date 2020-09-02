524d46b433c484edb9e014cbc297d91e0a5e3ef6
TBgames1 <tgbledsoe07@gmail.com>
2020-09-01 19:45:24 -0500

    Character Created and Running and Idle animations

---
+++ Assets/Camer_Rig.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camer_Rig : MonoBehaviour
+{
+    [SerializeField] Transform m_playerPosition; 
+    
+    void Update()
+    {
+        transform.position = m_playerPosition.position;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private bool m_isIdle = true;
+    private bool m_isRunning = false;
+    private Animator m_animator;
+    [SerializeField] float m_Speed = 7f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_isIdle)
+        {
+            m_animator.SetBool("isRunning", !m_isIdle);
+        }
+        if(Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        {
+         //   gameObject.transform.position = new Vector3();
+        }
+    }
+}
