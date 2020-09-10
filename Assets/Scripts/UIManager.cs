742fe4aef71a671eecdf3d764e439e4730c0cd51
T-Code07 <tgbledsoe07@gmail.com>
2020-09-10 01:42:15 -0500

    Revert "Built game for first time."
    This reverts commit 2b1ca53a84b8002963ec2eed21cd0b31d08f5850.

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -15,7 +15,6 @@ public class UIManager : MonoBehaviour
     [SerializeField] int m_restartScene = 0;
     [SerializeField] int m_MainMenu = 1;
     [SerializeField] TextMeshProUGUI m_mainMenuText;
-    [SerializeField] TextMeshProUGUI m_controlsText;
 
     //Muted:
     [SerializeField] TextMeshProUGUI m_mutedText;
@@ -24,7 +23,6 @@ public class UIManager : MonoBehaviour
     //General:
     public bool m_isPlayerDead = false;
     private bool m_isPaused = false;
-    private bool m_showingHelp = false;
 
     private void Start()
     {
