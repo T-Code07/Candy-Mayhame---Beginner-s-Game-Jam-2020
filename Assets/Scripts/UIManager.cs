03330adebdacbee72660935f303586b59c5997c5
T-Code07 <tgbledsoe07@gmail.com>
2020-09-10 00:33:39 -0500

    Player can pause by hitting "p."

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -8,11 +8,13 @@ public class UIManager : MonoBehaviour
 {
     //Health:
     [SerializeField] TextMeshProUGUI m_healthText;
+    [SerializeField] TextMeshProUGUI m_deadText;
 
     //Menu:
     [SerializeField] GameObject m_menu;
     [SerializeField] int m_restartScene = 0;
     [SerializeField] int m_MainMenu = 1;
+    [SerializeField] TextMeshProUGUI m_mainMenuText;
 
     //Muted:
     [SerializeField] TextMeshProUGUI m_mutedText;
@@ -20,18 +22,42 @@ public class UIManager : MonoBehaviour
 
     //General:
     public bool m_isPlayerDead = false;
+    private bool m_isPaused = false;
 
     private void Start()
     {
         Time.timeScale = 1f;
         m_mutedText.enabled = false;
+        m_deadText.enabled = false;
+        m_menu.active = false;
+        m_mainMenuText.enabled = false;
     }
     private void Update()
     {
         if (m_isPlayerDead == true)
         {
             m_menu.active = true;
+            m_deadText.enabled = true;
         }
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (m_isPaused)
+            {
+                m_isPaused = false;
+                Time.timeScale = 1f;
+                m_menu.active = false;
+                m_mainMenuText.enabled = false;
+            }
+            else
+            {
+                m_isPaused = true;
+                Time.timeScale = 0f;
+                m_menu.active = true;
+                m_mainMenuText.enabled = true;
+            }
+        }
+       
     }
 
     public IEnumerator MuteSoundTextDisplay(bool isMuted)
